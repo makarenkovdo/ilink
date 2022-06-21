58d26749b02b2e5423246fc14035dfd0bf08410e
Andrey Makarenkov <makarenkov.do@gmail.com>
2022-06-21 12:35:27 +0300

    refactor: styled homePage

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,12 @@
 import { useQuery } from "@apollo/client";
+import { styled } from "@mui/material";
 import { GET_ALL_SENTENCES } from "../shared/api/queries/get-all-sentences";
 import { GET_SENTENCE } from "../shared/api/queries/get-sentence";
 import { Puzzle } from "../widgets/puzzle";
+import { StyledHomePageContainer } from "./styles";
 
-export const HomePage = () => <Puzzle />;
+export const HomePage = () => (
+  <StyledHomePageContainer>
+    <Puzzle />
+  </StyledHomePageContainer>
+);
---
+++ src/pages/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const StyledHomePageContainer = styled("div")(() => ({
+  display: "grid",
+  width: "100vw",
+  height: "100vh",
+  justifyContent: "center",
+  alignContent: "center",
+}));
