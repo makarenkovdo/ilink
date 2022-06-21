82dbf2b5bf9161691351924fb99b386cff0c3a1a
Andrey Makarenkov <makarenkov.do@gmail.com>
2022-06-21 15:59:39 +0300

    feat: webpack minimyzer + gzip

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,10 @@
 // production config
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
+
+const TerserPlugin = require("terser-webpack-plugin");
 
 const commonConfig = require("./common");
 
@@ -16,4 +20,23 @@ module.exports = merge(commonConfig, {
     react: "React",
     "react-dom": "ReactDOM",
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: true,
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: true,
+      }),
+    ],
+  },
+  plugins: [
+    new CompressionPlugin({
+      algorithm: "gzip",
+    }),
+  ],
 });
